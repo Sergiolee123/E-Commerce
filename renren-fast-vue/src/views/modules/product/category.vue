2e48a2baf2f8e42f6bd2f075c6514ae415a6a8ef
Sergiolee123 <lee.sergio.hk@gmail.com>
2024-04-07 22:58:23 +0800

    update error handling for menu frontend

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -26,48 +26,79 @@
 </template>
 <script>
 export default {
-    data() {
-        return {
-            menus: [],
-            defaultProps: {
-                children: 'children',
-                label: 'name'
-            }
-        };
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    };
+  },
+  methods: {
+    handleNodeClick() {
+
     },
-    methods: {
-        getMenus() {
-            this.$http({
-                url: this.$http.adornUrl('/product/category/list/tree'),
-                method: 'get'
-            }).then(({ data }) => {
-                if (data && data.code === 0) {
-                    this.menus = data.data
-                    this.totalPage = data.data
-                } else {
-                    this.menus = []
-                    this.totalPage = 0
-                }
-                this.dataListLoading = false
-            })
-        },
-        append(data) {
-            const newChild = { id: id++, label: 'testtest', children: [] };
-            if (!data.children) {
-                this.$set(data, 'children', []);
-            }
-            data.children.push(newChild);
-        },
-        remove(node, data) {
-            const parent = node.parent;
-            const children = parent.data.children || parent.data;
-            const index = children.findIndex(d => d.id === data.id);
-            children.splice(index, 1);
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.menus = data.data
+          this.totalPage = data.data
+        } else {
+          this.menus = []
+          this.totalPage = 0
         }
+        this.dataListLoading = false
+      })
     },
-    created() {
-        this.getMenus();
+    append(data) {
+      const newChild = { id: id++, label: 'testtest', children: [] };
+      if (!data.children) {
+        this.$set(data, 'children', []);
+      }
+      data.children.push(newChild);
     },
+    remove(node, data) {
+
+
+      this.$confirm(`This will delete the menu item ${data.name}. Continue?`, 'Warning', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        let ids = [data.catId]
+        return this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        })
+      }).then(({ data }) => {
+
+        if (data && data.code === 0) {
+          this.$message({
+            type: 'success',
+            message: 'Delete completed'
+          });
+
+          this.getMenus();
+        } else {
+          throw new Error(data.code);
+        }
+      }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: 'Delete canceled'
+          });
+        });
+
+    }
+  },
+  created() {
+    this.getMenus();
+  },
 };
 </script>
 <style lang="">
